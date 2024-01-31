be618d01513205930f956a40144b5809b4e42714
SHAILESH BHAVE <t.shailesh.bhave@accenture.com>
2024-01-31 20:08:39 +0530

    Initial Stage of Game

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 'use strict';
 console.log(document.querySelector(".message").textContent);
 
+const randNum= Math.trunc((Math.random()*20)+1);
+let score=20;
+
+document.querySelector(".number").textContent =randNum;
+
 
 
 document.querySelector(".check").addEventListener('click', function(){
-    var val =document.querySelector(".guess").value;
-    console.log(val);
-    if(!val)
+    var guess =Number(document.querySelector(".guess").value);
+    console.log(guess);
+    if(!guess)
     {
         document.querySelector(".message").textContent="Not a Number";
+    }
+    else if(guess===randNum)
+    {
+        document.querySelector(".message").textContent="Correct!!! You Win!!!";
+    }
+    else if(guess>randNum && score >1)
+    {
+        document.querySelector(".message").textContent="Guess is too High!! Try AGAIN";
+        score--;
+        document.querySelector(".score").textContent =score;
+
+    }
+    else if(guess<randNum && score >1)
+    {
+        document.querySelector(".message").textContent="Guess is too Low!! Try AGAIN";
+        score--;
+        document.querySelector(".score").textContent =score;
+
+    }
+    else
+    {
+        document.querySelector(".message").textContent="You Lost!!!!";
+        document.querySelector(".score").textContent=0;
     }
 })
 
