ef7e2a28283ed0189cfc7253c2f912d5f6a11cdf
SHAILESH BHAVE <t.shailesh.bhave@accenture.com>
2024-01-31 18:12:03 +0530

    My Game starts

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+console.log(document.querySelector(".message").textContent);
+
+
+
+document.querySelector(".check").addEventListener('click', function(){
+    var val =document.querySelector(".guess").value;
+    console.log(val);
+    if(!val)
+    {
+        document.querySelector(".message").textContent="Not a Number";
+    }
+})
+
