8e69bd4c87939fcd822b65cf542566410ba244dc
SHAILESH BHAVE <t.shailesh.bhave@accenture.com>
2024-02-05 13:54:11 +0530

    A working game!! But can still do better with code!!

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 'use strict';
 console.log(document.querySelector(".message").textContent);
 
-const randNum= Math.trunc((Math.random()*20)+1);
+let randNum= Math.trunc((Math.random()*20)+1);
 let score=20;
+let highScore=0;
 
-document.querySelector(".number").textContent =randNum;
+document.querySelector(".again").addEventListener('click', function(){
+    randNum= Math.trunc((Math.random()*20)+1);
+    score=20;
+    document.querySelector(".message").textContent="Start guessing...";
+    document.querySelector("body").style.backgroundColor='black';
+    document.querySelector(".number").textContent ="?";
+    document.querySelector(".number").style.width='15rem';
+    document.querySelector(".score").textContent =20;
+    document.querySelector(".guess").value="";
 
-
+})
 
 document.querySelector(".check").addEventListener('click', function(){
     var guess =Number(document.querySelector(".guess").value);
@@ -19,13 +28,20 @@ document.querySelector(".check").addEventListener('click', function(){
     {
         document.querySelector(".message").textContent="Correct!!! You Win!!!";
         document.querySelector("body").style.backgroundColor='orange';
+        document.querySelector(".number").textContent =randNum;
         document.querySelector(".number").style.width='30rem';
+        if(score>highScore)
+        {
+            highScore=score;
+            document.querySelector(".highscore").textContent=highScore;
+        }
     }
     else if(guess>randNum && score >1)
     {
         document.querySelector(".message").textContent="Guess is too High!! Try AGAIN";
         score--;
         document.querySelector(".score").textContent =score;
+        //scoreComp=score;
 
     }
     else if(guess<randNum && score >1)
@@ -33,6 +49,7 @@ document.querySelector(".check").addEventListener('click', function(){
         document.querySelector(".message").textContent="Guess is too Low!! Try AGAIN";
         score--;
         document.querySelector(".score").textContent =score;
+        //scoreComp=score;
 
     }
     else
